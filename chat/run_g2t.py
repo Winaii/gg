830784b77d0248c6846f20d0e69ba2471e85ee42
Winaii <142558930+Winaii@users.noreply.github.com>
2026-03-14 12:30:38 +0800

    Refactor run_g2t.py to use command-line arguments

--- chat/run_g2t.py
+++ chat/run_g2t.py
@@ -1,21 +1,60 @@
+import argparse
 import json
+import torch
 from tqdm import tqdm
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
-    
+# Import config
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+from chat.config import Config
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Convert semantic graphs into descriptions using a seq2seq model.")
+    parser.add_argument(
+        "--input",
+        type=str,
+        default=Config.G2T_INPUT,
+        help="Input file containing semantic graphs, one per line.",
+    )
+    parser.add_argument(
+        "--output",
+        type=str,
+        default=Config.G2T_OUTPUT,
+        help="Output file to append generated descriptions to.",
+    )
+    parser.add_argument(
+        "--model",
+        type=str,
+        default=Config.G2T_MODEL,
+        help="HuggingFace model name or path.",
+    )
+    parser.add_argument(
+        "--device",
+        type=str,
+        default=Config.G2T_DEVICE,
+        help="Device to run the model on (e.g., cpu, cuda).",
+    )
+    return parser.parse_args()
+
+
+args = parse_args()
+
 text = []
-with open('data/GenWiki_source_graph/Iteration1/train.source', 'r') as f:
+with open(args.input, "r") as f:
     for l in f.readlines():
         text.append(l.strip())
 
-model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-xl").cuda()
-tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-xl")
+model = AutoModelForSeq2SeqLM.from_pretrained(args.model).to(args.device)
+tokenizer = AutoTokenizer.from_pretrained(args.model)
 
-with open("outputs/zero_shot_genwiki_results_all/flan_t5_xl_generated_train_I1.target","a") as output_file:
+with open(args.output, "a") as output_file:
     for i in tqdm(range(len(text))):
         prompt = "Transform the semantic graph into a description. Semantic graph: " + text[i]
-        inputs = tokenizer.encode(prompt, max_length=1024, truncation=False, return_tensors="pt").cuda()
+        inputs = tokenizer.encode(prompt, max_length=1024, truncation=False, return_tensors="pt").to(args.device)
         outputs = model.generate(inputs, max_length=1024)
         pred = tokenizer.decode(outputs[0].cpu(), skip_special_tokens=True)
-        output_file.write(pred.strip() + '\n')
+        output_file.write(pred.strip() + "\n")
 
